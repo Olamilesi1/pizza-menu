888a4d894520fecd2bf13c1cc7a4f3dfe25c2959
Olamilesi Animashaun <olamilesiadeola111@gmail.com>
2024-06-01 02:14:59 +0100

    pizza menu

---
+++ src/Profile.jsx
@@ -0,0 +1,72 @@
+// import { StrictMode } from "react";
+// import { createRoot } from "react-dom/client";
+import "./profile.css";
+
+// function App() {
+//   return (
+//     <div className="card">
+//       <Avatar />
+//       <div className="data">
+//         <Intro />
+//         {/* Should contain one Skill component
+//         for each web dev skill that you have,
+//         customized with props */}
+//         <div className="skill-list">
+//           <SkillList />
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
+// function Avatar() {
+//   return (
+//     <div className="avatar">
+//       <img src="" alt="developerImage" />
+//     </div>
+//   );
+// }
+
+// function Intro() {
+//   return (
+//     <div className="intro">
+//       <h1>Jonas Schemdtmann</h1>
+//       <p>
+//         Full-stack web developer and teacher at Udemy. When bot coding or
+//         preparing a course, I like to play board games, to cook (and eat), or
+//         just enjoy the Portugese sun at the beach.
+//       </p>
+//     </div>
+//   );
+// }
+
+// function SkillList() {
+//   return (
+//     <div className="skill-list">
+//       <Skill skillname="HTML + CSS" emoji="💪" color="blue" />
+//       <Skill skillname="JavaScript" emoji="💪" color="red" />
+//       <Skill skillname="Web Design" emoji="💪" color="orange" />
+//       <Skill skillname="Git and Github" emoji="💪" color="grey" />
+//       <Skill skillname="React" emoji="💪" color="yellow" />
+//       <Skill skillname="Svelte" emoji="👶" color="pink" />
+//     </div>
+//   );
+// }
+
+// function Skill({ skillname, emoji, color }) {
+//   return (
+//     <div className="skill" style={{ backgroundColor: color }}>
+//       <span> {skillname}</span>
+//       <span>{emoji}</span>
+//     </div>
+//   );
+// }
+
+// const rootElement = document.getElementById("root");
+// const root = createRoot(rootElement);
+
+// root.render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>
+// );
